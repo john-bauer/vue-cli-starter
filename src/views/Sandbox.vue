a6149de1ceae4ca9201d3ac7a00e248a124a64f3
John Bauer <johnbauer126@gmail.com>
2019-03-15 15:10:00 -0400

    added gsap example to start working with

--- src/views/Sandbox.vue
+++ src/views/Sandbox.vue
@@ -5,22 +5,39 @@
       subtitle="a place to mess around with the app adsfasdf"
       backgroundClass="is-dark"
     />
-    <button class="button" v-on:click="testEnv">asdf</button>
+    <PlaceholderCopy/>
+    <section class="section">
+      <div class="container">
+        <button class="button" ref="appButton">This is some button</button>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import { TimelineLite } from 'gsap';
 import Hero from '@/components/Hero.vue';
+import PlaceholderCopy from '@/components/PlaceholderCopy.vue';
 
 export default {
   name: 'sandbox',
   components: {
-    Hero
+    Hero,
+    PlaceholderCopy
   },
   methods: {
     testEnv() {
       console.log(process.env.VUE_APP_TITLE);
     }
+  },
+  mounted() {
+    const { appButton } = this.$refs;
+    const timeline = new TimelineLite();
+    console.log(timeline);
+    // (Element to animate, seconds to animate for, description of animation)
+    timeline.to(appButton, 1, { x: 200, rotation: 90 });
   }
 };
 </script>
+
+<style lang="scss"></style>
